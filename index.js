5f21f3f5200b3811133ad9020ca0371443146ac9
Ujjwal-FuseMachine <ujjwal.maharjan@fusemachines.com>
2023-07-25 17:35:59 +0545

    updated url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, function () {
     console.log(`app listening on port ${PORT}!`);
@@ -18,7 +18,3 @@ app.listen(PORT, function () {
 app.post("/openai/chatbot", supportChatbotRouter.generateSupportChatbotResponse);
 
 app.get('/test', (req, res) => { res.send("ok")});
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-})
